74aa0eaa159b5703fccc5cad81cca1711d78ecad
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2016-11-14 17:15:39 +0100

    [SignalR]: chat con websocket (raw)

---
+++ SignalR/Demo02/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(Demo02.Startup))]
+
+namespace Demo02
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+
+            app.UseWebApi(config);
+        }
+    }
+}

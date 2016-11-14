18f5db718900f57b821b03cbd6ea47adb671e12b
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2016-11-14 17:00:58 +0100

    [SignalR]: intro a websocket

---
+++ SignalR/Demo01/ChatController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.WebSockets;
+
+namespace Demo01
+{
+    [RoutePrefix("api/chat")]
+    public class ChatController : ApiController
+    {
+        [Route]
+        [HttpGet]
+        public HttpResponseMessage Get()
+        {
+            if (HttpContext.Current.IsWebSocketRequest)
+            {
+                HttpContext.Current.AcceptWebSocketRequest(ProcessWebSocketRequestAsync);
+            }
+
+            return new HttpResponseMessage(HttpStatusCode.SwitchingProtocols);
+        }
+
+        async Task ProcessWebSocketRequestAsync(AspNetWebSocketContext context)
+        {
+            var socket = context.WebSocket;
+            while (true)
+            {
+                var buffer = new ArraySegment<byte>(new byte[8192]);
+                var result = await socket.ReceiveAsync(buffer, CancellationToken.None);
+                if (socket.State == WebSocketState.Open)
+                {
+                    var message = $"You sent message: {Encoding.UTF8.GetString(buffer.Array, 0, result.Count)} at {DateTime.Now.ToString()}";
+                    await socket.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes(message)), WebSocketMessageType.Text, true, CancellationToken.None);
+                }
+                else
+                    break;
+            }
+        }
+    }
+}
---
+++ SignalR/Demo01/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(Demo01.Startup))]
+
+namespace Demo01
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+
+            app.UseWebApi(config);
+        }
+    }
+}

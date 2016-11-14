57369e3b5750228b7636cfac24121b972dd4bd2e
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2016-11-14 17:15:39 +0100

    [SignalR]: chat con websocket (raw)

---
+++ SignalR/Demo02/ChatController.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace Demo02
+{
+    [RoutePrefix("api/chat")]
+    public class ChatController : ApiController
+    {
+        [Route]
+        [HttpGet]
+        public HttpResponseMessage Get(string account)
+        {
+            if (HttpContext.Current.IsWebSocketRequest)
+            {
+                var handler = new ChatHandler(account);
+                HttpContext.Current.AcceptWebSocketRequest(context => handler.ProcessWebSocketRequestAsync(context));
+            }
+
+            return new HttpResponseMessage(HttpStatusCode.SwitchingProtocols);
+        }
+    }
+}
---
+++ SignalR/Demo02/ChatHandler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.WebSockets;
+
+namespace Demo02
+{
+    class ChatHandler
+    {
+        private static ConcurrentDictionary<string, AspNetWebSocketContext> clients = new ConcurrentDictionary<string, AspNetWebSocketContext>();
+
+        private readonly string account;
+
+        public ChatHandler(string account)
+        {
+            this.account = account;
+        }
+
+        public async Task ProcessWebSocketRequestAsync(AspNetWebSocketContext context)
+        {
+            var socket = context.WebSocket;
+
+            clients.AddOrUpdate(account, context, (account, current) => current);
+            while (true)
+            {
+                var buffer = new ArraySegment<byte>(new byte[8192]);
+                var result = await socket.ReceiveAsync(buffer, CancellationToken.None);
+                if (socket.State == WebSocketState.Open)
+                {
+                    var message = $"{account}: {Encoding.UTF8.GetString(buffer.Array, 0, result.Count)} [{DateTime.Now.ToString()}]";
+                    await Broadcast(message);
+                }
+                else
+                    break;
+            }
+        }
+
+        private Task Broadcast(string message)
+        {
+            var buffer = new ArraySegment<byte>(Encoding.UTF8.GetBytes(message));
+            var tasks = clients.Values.Select(context => context.WebSocket.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None));
+
+            return Task.WhenAll(tasks);
+        }
+    }
+}

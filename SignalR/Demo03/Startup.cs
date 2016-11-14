a0a200f6d370cad9da0540028f118efd7f19b742
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2016-11-14 21:59:54 +0100

    [SignalR]: introduzione a SignalR

---
+++ SignalR/Demo03/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Demo03.Startup))]
+
+namespace Demo03
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}

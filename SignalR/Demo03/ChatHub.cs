94561f426aae7f96259be85e00ecfc95c308a88a
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2016-11-14 21:59:54 +0100

    [SignalR]: introduzione a SignalR

---
+++ SignalR/Demo03/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+
+namespace Demo03
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string account, string message)
+        {
+            var content = $"<i>{account}</i>: <strong>{message}</strong> [{DateTime.Now.ToString()}]";
+            Clients.All.broadcast(content);
+        }
+    }
+}

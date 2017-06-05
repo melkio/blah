e912e37ab52edc80efa40ffe63d4aaa9271d3653
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2017-06-05 21:31:52 +0200

    [Akka.Net] Configured owin/webapi for HttpCache project

---
+++ Akka.Net/HttpCache/EchoController.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HttpCache
+{
+    [RoutePrefix("api/echo")]
+    public class EchoController : ApiController
+    {
+        [Route]
+        [HttpGet]
+        public HttpResponseMessage Get(string text)
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, new {Message = text});
+        }
+    }
+}
---
+++ Akka.Net/HttpCache/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(HttpCache.Startup))]
+
+namespace HttpCache
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+
+            app.UseWebApi(config);
+        }
+    }
+}
